ca4683187568a6c9f7dd253ac18a4d78d2c5f925
Eric Minassian <minassianeric2848@gmail.com>
2025-05-03 08:24:40 -0700

    feat: data table

--- src/pages/(dashboard)/accounts/page.tsx
+++ src/pages/(dashboard)/accounts/page.tsx
@@ -1,4 +1,7 @@
+import { DataTable } from "@/components/data-table";
+import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { ContentLayout } from "@/components/ui/content-layout";
 import {
   DropdownMenu,
@@ -7,16 +10,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Header } from "@/components/ui/header";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { useDB } from "@/hooks/db";
 import { Account, accountsTable } from "@/lib/db/schema";
+import { ColumnDef } from "@tanstack/react-table";
 import { eq } from "drizzle-orm";
 import { MoreHorizontal } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -55,6 +51,86 @@ export default function AccountsPage() {
     }
   }
 
+  const columns: ColumnDef<Account>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "id",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Id" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <Link
+            href={`/accounts/${row.original.id}`}
+            className="underline-offset-4 hover:underline"
+          >
+            {row.original.id}
+          </Link>
+        );
+      },
+    },
+    {
+      accessorKey: "name",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Name" />
+      ),
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <Link href={`/accounts/${row.original.id}/edit`} asChild>
+                <DropdownMenuItem>Edit</DropdownMenuItem>
+              </Link>
+
+              <DropdownMenuItem
+                variant="destructive"
+                onClick={async () => {
+                  if (
+                    confirm("Are you sure you want to delete this account?")
+                  ) {
+                    handleDeleteAccount(row.original.id);
+                  }
+                }}
+              >
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
   if (loading) {
     return <div className="p-4 text-center">Loading...</div>;
   }
@@ -74,58 +150,7 @@ export default function AccountsPage() {
         </Header>
       }
     >
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {accounts.map((account) => (
-            <TableRow key={account.id}>
-              <TableCell className="font-medium">
-                <Button variant="link" className="p-0" asChild>
-                  <Link href={`/accounts/${account.id}`}>{account.id}</Link>
-                </Button>
-              </TableCell>
-
-              <TableCell>{account.name}</TableCell>
-              <TableCell className="text-right">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <span className="sr-only">Open menu</span>
-                      <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <Link href={`/accounts/${account.id}/edit`} asChild>
-                      <DropdownMenuItem>Edit</DropdownMenuItem>
-                    </Link>
-
-                    <DropdownMenuItem
-                      className="text-destructive"
-                      onClick={() => {
-                        if (
-                          confirm(
-                            "Are you sure you want to delete this account?"
-                          )
-                        ) {
-                          handleDeleteAccount(account.id);
-                        }
-                      }}
-                    >
-                      Delete
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <DataTable data={accounts} columns={columns} searchColumn="name" />
     </ContentLayout>
   );
 }
