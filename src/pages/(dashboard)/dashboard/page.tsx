4e2035b1c9c3a3af77ae9c6d8de2627091f5699b
Eric Minassian <minassianeric2848@gmail.com>
2025-05-08 20:18:34 -0700

    feat: simple dashboard

--- src/pages/(dashboard)/dashboard/page.tsx
+++ src/pages/(dashboard)/dashboard/page.tsx
@@ -1,17 +1,149 @@
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import { ContentLayout } from "@/components/ui/content-layout";
 import { Header } from "@/components/ui/header";
+import { useDB } from "@/hooks/db";
+import { transactionsTable } from "@/lib/db/schema/transaction";
+import currency from "currency.js";
+import { sum } from "drizzle-orm";
+import { useEffect, useState } from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const chartConfig = {
+  amount: {
+    label: "Amount",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
 
 export default function DashboardPage() {
+  const { db } = useDB();
+  const [continuousAccountsSum, setContinuousAccountsSum] = useState<
+    { date: string; amount: number }[]
+  >([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const accounts = await db!
+        .select({
+          date: transactionsTable.date,
+          amount: sum(transactionsTable.amount),
+        })
+        .from(transactionsTable)
+        .groupBy(transactionsTable.date)
+        .orderBy(transactionsTable.date);
+
+      const totalSums = [];
+      let currentTotal = 0;
+
+      for (const account of accounts) {
+        currentTotal += account.amount !== null ? Number(account.amount) : 0;
+        totalSums.push({
+          date: account.date.toString(),
+          amount: currentTotal,
+        });
+      }
+
+      setContinuousAccountsSum(totalSums);
+    }
+
+    fetchData();
+  }, [db]);
+
+  function gradientOffset() {
+    const dataMax = Math.max(...continuousAccountsSum.map((i) => i.amount));
+    const dataMin = Math.min(...continuousAccountsSum.map((i) => i.amount));
+
+    if (dataMax <= 0) {
+      return 0;
+    }
+    if (dataMin >= 0) {
+      return 1;
+    }
+
+    return dataMax / (dataMax - dataMin);
+  }
+
+  const off = gradientOffset();
+
   return (
     <ContentLayout header={<Header>Dashboard</Header>}>
-      <div className="flex flex-col gap-4 p-4 flex-1">
-        <div className="grid gap-4 md:grid-cols-3 auto-rows-min">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="rounded-xl bg-muted/50 flex-1 md:min-h-min min-h-[50vh]" />
-      </div>
+      <ChartContainer
+        config={chartConfig}
+        className="aspect-auto h-[250px] w-full"
+      >
+        <AreaChart
+          data={continuousAccountsSum}
+          accessibilityLayer
+          margin={{
+            left: 12,
+            right: 12,
+          }}
+        >
+          <defs>
+            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+              <stop offset={off} stopColor="green" stopOpacity={1} />
+              <stop offset={off} stopColor="red" stopOpacity={1} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="date"
+            tickLine={false}
+            axisLine={false}
+            tickMargin={8}
+            tickFormatter={(value) => {
+              const date = new Date(value);
+              return date.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+              });
+            }}
+          />
+          <YAxis
+            tickLine={false}
+            axisLine={false}
+            tickMargin={8}
+            tickFormatter={(value) => {
+              return currency(value.toString(), {
+                fromCents: true,
+              }).format();
+            }}
+          />
+          <ChartTooltip
+            content={
+              <ChartTooltipContent
+                labelFormatter={(value) => {
+                  const date = new Date(value);
+                  return date.toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  });
+                }}
+                formatter={(value) => {
+                  return currency(value.toString(), {
+                    fromCents: true,
+                  }).format();
+                }}
+              />
+            }
+          />
+          <Area type="monotone" dataKey="amount" fill="url(#splitColor)" />
+          <ReferenceLine y={0} stroke="#000" strokeDasharray="3 3" />
+        </AreaChart>
+      </ChartContainer>
     </ContentLayout>
   );
 }
