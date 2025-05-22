053170fa1928ba027c7019af89611debb4dc5b7c
Eric Minassian <minassianeric2848@gmail.com>
2025-05-21 22:08:55 -0700

    feat: rules scaffolding

---
+++ src/lib/services/rules/list-rules.ts
@@ -0,0 +1,46 @@
+import {
+  RuleStatement,
+  ruleStatementsTable,
+} from "@/lib/db/schema/rule-statements";
+import { Rule, rulesTable } from "@/lib/db/schema/rules";
+import { Database } from "@/lib/types";
+import { eq, sql } from "drizzle-orm";
+
+interface ListRulesRequest {
+  db: Database;
+}
+
+type ListRulesResponse = Array<
+  Rule & {
+    statements: Array<RuleStatement>;
+  }
+>;
+
+export async function listRules({
+  db,
+}: ListRulesRequest): Promise<ListRulesResponse> {
+  const results = await db
+    .select({
+      id: rulesTable.id,
+      updateField: rulesTable.updateField,
+      updateValue: rulesTable.updateValue,
+      statements: sql<string>`json_group_array(
+      json_object(
+        'id', ${ruleStatementsTable.id},
+        'field', ${ruleStatementsTable.field},
+        'operator', ${ruleStatementsTable.operator},
+        'value', ${ruleStatementsTable.value}
+      )
+    )`,
+    })
+    .from(rulesTable)
+    .innerJoin(
+      ruleStatementsTable,
+      eq(rulesTable.id, ruleStatementsTable.ruleId)
+    );
+
+  return results.map((row) => ({
+    ...row,
+    statements: JSON.parse(row.statements),
+  }));
+}
