169ae72207f15a9fcb6c94bfedc216e57001ba42
Eric Minassian <minassianeric2848@gmail.com>
2025-07-05 16:08:35 -0700

    test: create transaction

--- e2e/page-objects/AccountPage.ts
+++ e2e/page-objects/AccountPage.ts
@@ -7,9 +7,20 @@ export class AccountPage {
     await this.page.getByRole("link", { name: "Accounts" }).click();
   }
 
+  async goToAccount(name: string) {
+    await this.page.getByRole("link", { name }).click();
+  }
+
   async createAccount(name: string) {
     await this.page.getByRole("button", { name: "Add Account" }).click();
     await this.page.getByLabel("Name").fill(name);
     await this.page.getByRole("button", { name: "Create Account" }).click();
+  }
+
+  async createTransaction(payee: string, amount: string) {
+    await this.page.getByRole("button", { name: "Create Transaction" }).click();
+    await this.page.getByLabel("Payee").fill(payee);
+    await this.page.getByLabel("Amount").fill(amount);
+    await this.page.getByRole("button", { name: "Create Transaction" }).click();
   }
 }
--- e2e/specs/settings/categories.spec.ts
+++ e2e/specs/settings/categories.spec.ts
@@ -2,29 +2,59 @@ import { expect, test } from "../../fixtures";
 
 test.describe("Categories", () => {
   const categoryName = "Test Category";
+  const databasePassword = "testpassword";
 
   test.beforeEach(async ({ page, databasePage, settingsPage }) => {
     await page.goto("/");
-    await databasePage.createEncryptedDatabase("testpassword");
+    await databasePage.createEncryptedDatabase(databasePassword);
     await settingsPage.goToCategories();
   });
 
-  test("create category", async ({ page, settingsPage }) => {
+  test("create category", async ({
+    page,
+    settingsPage,
+    databasePage,
+    dashboardPage,
+  }) => {
     const categoryLocator = settingsPage.categoryLocator(categoryName);
     await expect(categoryLocator).toBeHidden();
 
     await settingsPage.createCategory(categoryName);
+    await expect(categoryLocator).toBeVisible();
 
+    await dashboardPage.goto();
+    await settingsPage.goToCategories();
+    await expect(categoryLocator).toBeVisible();
+
+    const path = await databasePage.exportDatabase();
+    await page.reload();
+    await databasePage.importDatabase(path, databasePassword);
+    await settingsPage.goToCategories();
     await expect(categoryLocator).toBeVisible();
   });
 
-  test("delete category", async ({ page, settingsPage }) => {
+  test("delete category", async ({
+    page,
+    settingsPage,
+    databasePage,
+    dashboardPage,
+  }) => {
     const categoryLocator = settingsPage.categoryLocator(categoryName);
 
     await settingsPage.createCategory(categoryName);
     await expect(categoryLocator).toBeVisible();
 
     await categoryLocator.getByRole("button").click();
+    await expect(categoryLocator).toBeHidden();
+
+    await dashboardPage.goto();
+    await settingsPage.goToCategories();
+    await expect(categoryLocator).toBeHidden();
+
+    const path = await databasePage.exportDatabase();
+    await page.reload();
+    await databasePage.importDatabase(path, databasePassword);
+    await settingsPage.goToCategories();
     await expect(categoryLocator).toBeHidden();
   });
 });
